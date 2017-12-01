2350b9566c09ef0749e193ef4d4510f4658ef5c8
zgb <zgbdsg@gmail.com>
2017-12-01 10:03:42 +0800

    remove star rate

--- static/scripts/product/sign_read.js
+++ static/scripts/product/sign_read.js
@@ -503,6 +503,7 @@
 				play.controlIsShow = true;
 
 				var vtype = $("#type").text();
+
 				if(vtype != "2"){
 					$( play.controlBox ).slideDown(600);
 				}
@@ -959,6 +960,7 @@
 
 					play.controlIsShow = true;
 					var vtype = $("#type").text();
+
 					if(vtype != "2"){
 						$( play.controlBox ).slideDown(600);
 					}
@@ -1351,3 +1353,69 @@
 	window.addEventListener( "load", play.init );
 
 }();
+
+
+$(document).on('ready', function () {
+        $('.kv-gly-star').rating({
+            containerClass: 'is-star'
+        });
+        $('.kv-gly-heart').rating({
+            containerClass: 'is-heart',
+            defaultCaption: '{rating} hearts',
+            starCaptions: function (rating) {
+                return rating == 1 ? 'One heart' : rating + ' hearts';
+            },
+            filledStar: '<i class="glyphicon glyphicon-heart"></i>',
+            emptyStar: '<i class="glyphicon glyphicon-heart-empty"></i>'
+        });
+        $('.kv-fa').rating({
+            theme: 'krajee-fa',
+            filledStar: '<i class="fa fa-star"></i>',
+            emptyStar: '<i class="fa fa-star-o"></i>'
+        });
+        $('.kv-fa-heart').rating({
+            defaultCaption: '{rating} hearts',
+            starCaptions: function (rating) {
+                return rating == 1 ? 'One heart' : rating + ' hearts';
+            },
+            theme: 'krajee-fa',
+            filledStar: '<i class="fa fa-heart"></i>',
+            emptyStar: '<i class="fa fa-heart-o"></i>'
+        });
+        $('.kv-uni-star').rating({
+            theme: 'krajee-uni',
+            filledStar: '&#x2605;',
+            emptyStar: '&#x2606;'
+        });
+        $('.kv-uni-rook').rating({
+            theme: 'krajee-uni',
+            defaultCaption: '{rating} rooks',
+            starCaptions: function (rating) {
+                return rating == 1 ? 'One rook' : rating + ' rooks';
+            },
+            filledStar: '&#9820;',
+            emptyStar: '&#9814;'
+        });
+        $('.kv-svg').rating({
+            theme: 'krajee-svg',
+            filledStar: '<span class="krajee-icon krajee-icon-star"></span>',
+            emptyStar: '<span class="krajee-icon krajee-icon-star"></span>'
+        });
+        $('.kv-svg-heart').rating({
+            theme: 'krajee-svg',
+            filledStar: '<span class="krajee-icon krajee-icon-heart"></span>',
+            emptyStar: '<span class="krajee-icon krajee-icon-heart"></span>',
+            defaultCaption: '{rating} hearts',
+            starCaptions: function (rating) {
+                return rating == 1 ? 'One heart' : rating + ' hearts';
+            },
+            containerClass: 'is-heart'
+        });
+
+        $('.rating,.kv-gly-star,.kv-gly-heart,.kv-uni-star,.kv-uni-rook,.kv-fa,.kv-fa-heart,.kv-svg,.kv-svg-heart').on(
+                'change', function () {
+                    console.log('Rating selected: ' + $(this).val());
+                });
+    });
+
+
